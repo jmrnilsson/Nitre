655e7c8ccf50737befabea07729bf4796eb54a5b
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-10-17 22:11:37 +0200

    add 3-tuple to starmap

--- Itertools/Starmap.cs
+++ Itertools/Starmap.cs
@@ -14,5 +14,14 @@ namespace Itertools
         {
             return iterable.Select(t => valueFactory(t.Item1, t.Item2));
         }
+
+        public static IEnumerable<TResult> Starmap<T1, T2, T3, TResult>
+        (
+            Func<T1, T2, T3, TResult> valueFactory,
+            IEnumerable<Tuple<T1, T2, T3>> iterable
+        )
+        {
+            return iterable.Select(t => valueFactory(t.Item1, t.Item2, t.Item3));
+        }
     }
 }
--- Itertools/Tests/StarmapTests.cs
+++ Itertools/Tests/StarmapTests.cs
@@ -1,17 +1,34 @@
 ﻿using System;
+using System.Linq;
 using Xunit;
 
 namespace Itertools.Tests
 {
+    internal delegate dynamic Area(string name, int width, decimal height);
+
     public class StarmapTests
     {
         [Fact]
-        public void StarmapWithLambda()
+        public void Starmap()
         {
             var iterable0 = new[] {Tuple.Create(1, "first"), Tuple.Create(3, "second")};
             var actual = Itertools.Starmap((a, b) => $"{a}{b}", iterable0);
 
             Assert.StrictEqual("1first3second", string.Join("", actual));
         }
+
+        [Fact]
+        public void StarmapWithLambda()
+        {
+            Func<string, int, decimal, dynamic> action = (name, width, height) => new { name, area = width * height };
+            var iterable0 = new[] {Tuple.Create("A", 101, 1.2m), Tuple.Create("B", 3, .3m)};
+            var actual = Itertools.Starmap(action, iterable0).ToList();
+
+            Assert.Equal("A", actual[0].name);
+            Assert.Equal(101 * 1.2m, actual[0].area);
+            Assert.Equal("B", actual[1].name);
+            Assert.Equal(3 * .3m, actual[1].area);
+        }
+
     }
 }
